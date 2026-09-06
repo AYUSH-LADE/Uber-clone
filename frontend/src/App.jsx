ea85d0bdab6659e9ebb45e1185e8a9a40b77e041
AYUSH-LADE <257463501+AYUSH-LADE@users.noreply.github.com>
2026-09-06 19:30:39 +0530

    home page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import login from './pages/userlogin'
+import UserLogin from './pages/userlogin'
+import userSignup from './pages/userSignup'
 import Captainlogin from './pages/Captainlogin'
 import CaptainSignup from './pages/CaptainSignup'
-
 
 const App = () => {
   return (
     <div>
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<userlogin />} /> 
-            <Route path='/signup' element={<userSignup />} />
-            <Route path='/captain-login' element={<Captainlogin />} />
-            <Route path='/captain-Signup' element={<CaptainSignup />} />
-        </Routes>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<UserLogin />} />
+        <Route path='/signup' element={<userSignup />} />
+        <Route path='/captain-login' element={<Captainlogin />} />
+        <Route path='/captain-Signup' element={<CaptainSignup />} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/pages/userlogin.jsx
+++ frontend/src/pages/userlogin.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const userlogin = () => {
+const UserLogin = () => {  
   return (
-    <div>userlogin</div>
-  );
+    <div className='p-7'>
+      <form>
+        <h3 className='text-xl mb-2'>What's your email?</h3>
+        <input required className='bg-white rounded px-4 py-2 border w-full text-lg placeholder:text-xm' type="email" placeholder='Enter your email' />
+        <h3>What's your password?</h3>
+        <input required type="password" placeholder='Enter your password' />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  ); 
 }
 
-export default userlogin;
+export default UserLogin;   
